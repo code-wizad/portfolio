dd52b2bb52be13eba2718ff2c0ab3d49a054dde6
code-wizad <sirsociallogin@gmail.com>
2024-07-13 08:24:55 +0530

    moved contents in this file from another

---
+++ js/index.js
@@ -0,0 +1,9 @@
+window.onscroll = function() { updateProgressBar() };
+
+function updateProgressBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progressbar").style.width = scrolled + "%";
+    document.getElementById("progressvalue").textContent = math.round(scrolled) + "%";
+}
